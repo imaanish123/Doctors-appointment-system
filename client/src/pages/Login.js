cd33f0e809ddfbfc03bff25d328e23b3045f6430
Manish Kumar <mk1849253@gmail.com>
2024-02-18 17:31:51 +0530

    enviroment set and database connected

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,41 @@
 import React from "react";
+import "../styles/RegisterStyles.css";
+import { Form, Input } from "antd";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  // form handler
+  const onfinishHandler = (values) => {
+    console.log(values);
+  };
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+    <>
+      <div className="form-container">
+        <Form
+          layout="vertical"
+          onFinish={onfinishHandler}
+          className="register-form"
+        >
+          <h3 className="text-center">Login Form</h3>
+          {/* <Form.Item label="Name" name="name">
+            <Input type="text" required></Input>
+          </Form.Item> */}
+          <Form.Item label="Email" name="email">
+            <Input type="email" required></Input>
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input type="password" required></Input>
+          </Form.Item>
+          <Link to="/register" className="ms-2">
+            Not a User login here
+          </Link>
+
+          <button className="btn btn-primary" type="submit">
+            Login
+          </button>
+        </Form>
+      </div>
+    </>
   );
 };
 
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,14 +1,22 @@
 import React from "react";
-import { Button, Form, Input } from "antd";
+import "../styles/RegisterStyles.css";
+import { Form, Input } from "antd";
+import { Link } from "react-router-dom";
 
 const Register = () => {
+  // form handler
   const onfinishHandler = (values) => {
     console.log(values);
   };
   return (
     <>
       <div className="form-container">
-        <Form layout="vertical" onFinish={onfinishHandler}>
+        <Form
+          layout="vertical"
+          onFinish={onfinishHandler}
+          className="register-form"
+        >
+          <h3 className="text-center">Register Form</h3>
           <Form.Item label="Name" name="name">
             <Input type="text" required></Input>
           </Form.Item>
@@ -18,7 +26,15 @@ const Register = () => {
           <Form.Item label="Password" name="password">
             <Input type="password" required></Input>
           </Form.Item>
-          <Button>Register</Button>
+          <Link to="/login" className="ms-2">
+            Already User login here
+          </Link>
+          {/* <button className="btn btn-primary" type="submit">
+            Register
+          </button> */}
+          <button className="btn btn-primary" type="submit">
+            Register
+          </button>
         </Form>
       </div>
     </>
